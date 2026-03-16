a03dbd163ea11c2c771bd429cd4d5bd853ba3ed4
Torben Jensen <github@tjb.dk>
2026-03-16 20:04:28 +0100

    Update Draggable.js

--- src/dom/Draggable.js
+++ src/dom/Draggable.js
@@ -19,18 +19,16 @@ import * as PointerEvents from './DomEvent.PointerEvents.js';
  * ```
  */
 
-export class Draggable extends Evented {
+export const Draggable = Evented.extend({
 
-	static {
-		this.setDefaultOptions({
-			// @section
-			// @aka Draggable options
-			// @option clickTolerance: Number = 3
-			// The max number of pixels a user can shift the pointer during a click
-			// for it to be considered a valid click (as opposed to a pointer drag).
-			clickTolerance: 3
-		});
-	}
+	options: {
+		// @section
+		// @aka Draggable options
+		// @option clickTolerance: Number = 3
+		// The max number of pixels a user can shift the pointer during a click
+		// for it to be considered a valid click (as opposed to a pointer drag).
+		clickTolerance: 3
+	},
 
 	// @constructor Draggable(el: HTMLElement, dragHandle?: HTMLElement, preventOutline?: Boolean, options?: Draggable options)
 	// Creates a `Draggable` object for moving `el` when you start dragging the `dragHandle` element (equals `el` itself by default).
